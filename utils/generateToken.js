705223e74f870e1cc68b735214edc3d8e3cfe1e0
Anelka MD <anelkabagalaneofficiel@gmail.com>
2025-03-29 15:00:16 +0100

    update

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,11 +1,12 @@
 import jwt from "jsonwebtoken"
 
 // Fonction pour générer un token JWT
-const generateToken = (id) => {
+export const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE,
     })
 }
 
+// On exporte également par défaut pour maintenir la compatibilité
 export default generateToken
 
