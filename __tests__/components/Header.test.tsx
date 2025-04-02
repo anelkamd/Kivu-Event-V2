7c5f5ac6ad9311c646d3c6b6b15a5e526f409a6d
Anelka MD <anelkabagalaneofficiel@gmail.com>
2025-04-02 15:46:22 +0200

    test

--- __tests__/components/Header.test.tsx
+++ __tests__/components/Header.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
-import Header from "@/components/layout/Header"
+import Header from "@/components/layout/NavBar"
 import { AuthProvider } from "@/context/AuthContext"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
