024ce4425582ef0f2bc2cae83fed88a5d6bfb865
Anelka MD <anelkabagalaneofficiel@gmail.com>
2025-04-05 16:29:55 +0200

    update

--- app/api/events/id/participants/route.ts
+++ app/api/events/id/participants/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
-import QRCode from "qrcode";
+import { v4 as uuidv4 } from 'uuid'
+import QRCode from 'qrcode';
 import db from "@/lib/db";
 
 export async function GET(
