cd7188a73153b56bc367f20e5ec9cf7be38547a6
Anelka MD <anelkabagalaneofficiel@gmail.com>
2025-06-16 16:20:23 +0200

    fix bug regenate token

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,12 +1,11 @@
-import jwt from "jsonwebtoken"
+// utils/generateToken.js
+import jwt from "jsonwebtoken";
 
-// Fonction pour générer un token JWT
-export const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRE,
-    })
-}
-
-// On exporte également par défaut pour maintenir la compatibilité
-export default generateToken
+export const generateToken = (userId) => {
+  return jwt.sign(
+    { id: userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+};
 
