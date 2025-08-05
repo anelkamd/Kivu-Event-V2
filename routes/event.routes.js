a5503d56287691b7f67be172c51e8d35960c4b5a
Anelka MD <anelkabagalaneofficiel@gmail.com>
2025-08-05 22:04:36 +0200

    modification du models (events)

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,10 +1,12 @@
 import express from "express"
 import eventController from "../controllers/eventController.js"
+// Correction du chemin d'importation : utilisez le nom de fichier correct 'auth.middleware.js'
+import { protect } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
 // Routes CRUD pour les événements
-router.post("/create", (req, res) => eventController.createEvent(req, res))
+router.post("/create", protect, (req, res) => eventController.createEvent(req, res))
 router.get("/", (req, res) => eventController.getAllEvents(req, res))
 router.get("/:id", (req, res) => eventController.getEventById(req, res))
 router.put("/:id", (req, res) => eventController.updateEvent(req, res))
