092bcd959181775086711d4dc9f8c5f621e43b44
Anelka MD <anelkabagalaneofficiel@gmail.com>
2025-09-01 18:10:24 +0200

    correction des erreur du prod

--- app/api/debug/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET() {
-    return NextResponse.json({
-        success: true,
-        message: "Le point d'accès API fonctionne",
-        time: new Date().toISOString(),
-    })
-}
